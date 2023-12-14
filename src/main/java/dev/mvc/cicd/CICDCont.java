5526621f3a14d920b088624e7509c74040fac88e
hyeonsu6 <bhshs0508@naver.com>
2023-12-14 15:28:21 +0900

    CICD 테스트

--- src/main/java/dev/mvc/cicd/CICDCont.java
+++ src/main/java/dev/mvc/cicd/CICDCont.java
@@ -9,32 +9,33 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 public class CICDCont {
-  // http://localhost:9091/cicd?sw=1
-  // http://localhost:9091/cicd?sw=-1
-  // http://localhost:9091/cicd?sw=0
-  @GetMapping("/cicd")
-  public String cicd(@RequestParam(defaultValue = "1") Integer sw) {
-    String msg = "";
-    
-    if (sw == 1) {
-      msg = "/cicd 호출됨, info 로그";
-      log.info(msg);
-    } else if (sw == -1) {
-      msg = "/cicd 호출됨, error 로그";
-      log.error(msg);
-    } else if (sw == 0) {
-      msg = "/cicd 호출됨, warn 로그";
-      log.warn(msg);
-    } else if (sw == 2) {
-      msg = "/cicd workflow 테스트";
-      log.info(msg);
-    } else if (sw == 3) {
-        msg = "/cicd Github action + EC2 최종 테스트";
-        log.info(msg);
-      }
+	// http://localhost:9091/cicd?sw=1
+	// http://localhost:9091/cicd?sw=-1
+	// http://localhost:9091/cicd?sw=0
+	@GetMapping("/cicd")
+	public String cicd(@RequestParam(defaultValue = "1") Integer sw) {
+		String msg = "";
 
-    
-    return "<h3>" + msg + "</h3>";
-  }
+		if (sw == 1) {
+			msg = "/cicd 호출됨, info 로그";
+			log.info(msg);
+		} else if (sw == -1) {
+			msg = "/cicd 호출됨, error 로그";
+			log.error(msg);
+		} else if (sw == 0) {
+			msg = "/cicd 호출됨, warn 로그";
+			log.warn(msg);
+		} else if (sw == 2) {
+			msg = "/cicd workflow 테스트";
+			log.info(msg);
+		} else if (sw == 3) {
+			msg = "/cicd Github action + EC2 최종 테스트";
+			log.info(msg);
+		} else if (sw == 4) {
+			msg = "/cicd Github action + EC2 최종 테스트 2";
+			log.info(msg);
+		}
+
+		return "<h3>" + msg + "</h3>";
+	}
 }
- 
